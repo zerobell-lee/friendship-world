65fa16a935cc725904a1f039529db025c75e87f5
Zerobell Lee <lee@zerobell.xyz>
2023-01-04 04:38:15 +0900

    Initial Commit

---
+++ src/main/kotlin/xyz/zerobell/webgameproject/WebGameProjectApplication.kt
@@ -0,0 +1,11 @@
+package xyz.zerobell.webgameproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebGameProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebGameProjectApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/zerobell/webgameproject/WebGameProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.zerobell.webgameproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebGameProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
